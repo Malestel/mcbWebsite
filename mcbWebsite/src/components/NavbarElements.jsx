22251c33805badba1b7b653745e0cac6758b42cb
Malestel <malestel-48@hotmail.com>
2025-06-16 18:17:38 -0500

    Added Nav Bar
    Added a Nav Bar and updated About Us Page

---
+++ mcbWebsite/src/components/NavbarElements.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { Navbar, Nav } from 'react-bootstrap';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
+import About from '../pages/About';
+import Home from '../pages/Home';
+
+export default class NavbarComp extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+
+                    <Navbar bg="dark" fixed="top" variant={"dark"} expand="lg">
+                        <Navbar.Brand href="#">Madison Community Band</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="mr-auto my-2 my-lg-0"
+                                style={{ maxHeight: '100px' }}
+                                navbarScroll>
+
+                                <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                                <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+
+
+                            </Nav>
+
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<Home />} />  {/* Default route */}
+                        <Route path="/about" element={<About />}/>
+                        <Route path="/home" element={<Home />}/>
+                    </Routes>
+                </div>
+            </Router>
+        )
+    }
+}
---
+++ mcbWebsite/src/pages/About.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <br></br>
+        <h1>Who Are We?</h1>
+        <h4>
+          Founded in 1993, the Madison Community Band continues a long tradition
+          of community bands by providing musical performance opportunities for
+          its members and promoting and encouraging the arts community in
+          Madison county and surrounding communities.  The band, which has grown
+          from fifteen members to sixty-plus members, performs annually at
+          community-wide events in Huntsville, Decatur and Guntersville,
+          Alabama, as well as Fayetteville, Tennessee. Aptly named, the Madison
+          Community Band is truly a “community” band. The band’s members come
+          from a variety of backgrounds and represent a wide-range of ages, from
+          middle school students to retirees. Nearly all of the band’s members
+          have had previous ensemble experience, whether in high school, college
+          or even professional ensembles. For many of the members, the Madison
+          Community Band allows them to continue playing their instruments in a
+          band setting, an opportunity that they would not have without the
+          community band. The Madison Community Band is an all-volunteer
+          organization uniting individuals of different ages and backgrounds for
+          a common passion – musical performance!{" "}
+        </h4>
+
+        <br></br>
+        <h1>Officers and Board of Directors</h1>
+        <h4>President – Vacant</h4>
+        <h4>Vice President – Sara Ann Emerson</h4>
+        <h4>Secretary/Treasurer – Robert Schwartz</h4>
+        <h4>Librarian – Samuel Strong</h4>
+        <h4>Music Director and Conductor– Dave Ryan</h4>
+
+        <br></br>
+        <h1>Advisors to the Board</h1>
+        <h4>Student Representative- Vacant</h4>
+
+        <br></br>
+        <h1>Scholarship Program</h1>
+        <h4>
+          The Madison Community Band honors the contributions of its youngest
+          members through a scholarship program. The $500 scholarship is awarded
+          annually to a junior or senior student.
+        </h4>
+      </div>
+    );
+  }
+}
