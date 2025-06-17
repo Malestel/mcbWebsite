bd2c73d9f1dcee72d1080a7b9cc5d095230b5436
Malestel <malestel-48@hotmail.com>
2025-06-17 00:25:37 -0500

    Home Page Rework

--- mcbWebsite/src/pages/Home.jsx
+++ mcbWebsite/src/pages/Home.jsx
@@ -1,12 +1,176 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./css/Home.css";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home</h1>
-                <p>This is home.</p>
+  render() {
+    return (
+      <div>
+        <div className="image-container">
+          <img
+            src="./src/assets/flagPicCrop.jpg"
+            alt="Madison Community Band"
+          />
+        </div>
+        <br></br>
+        <br></br>
+        <div className="main-content">
+          <div className="bodyText">
+            The Madison Community Band, a 501(c)(3) Nonprofit Public Charity,
+            based in North Alabama, follows a long tradition of community bands.
+            An all-volunteer organization, the band is comprised of musicians of
+            all ages and backgrounds. The Madison Community Band performs
+            annually for various events throughout the Tennessee Valley,
+            including Huntsville’s Panoply of the Arts, Madison’s Gazebo
+            Concerts, Decatur’s Concerts by the River, and the Host of Christmas
+            Past Festival in Fayetteville , TN. We have also been honored to
+            perform at the US Space and Rocket Center, the Lakeside Summer
+            Concert Series in Guntersville and at Guntersville State Park.
+          </div>
+          <br></br>
+          <br></br>
+          <h1>See MCB In Concert!</h1>
+          <div class="columnList-container">
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
             </div>
-        )
-    }
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+          </div>
+          <h1>Come Rehearse With Us!</h1>
+          <div class="columnList-container">
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+            <div class="columnList-item">
+              <div class="columnList-date">July 29, 2025</div>
+              <div class="columnList-location">
+                Grace United Methodist Church
+              </div>
+              <div class="columnList-location">Huntsville, AL</div>
+              <div class="columnList-location">Call time – 6:30PM</div>
+              <div class="columnList-location">Performance time – 7:00PM</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
